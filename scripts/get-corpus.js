ea3f710398d3450734cbe2a97f0178bf4aee097a
redaphid <iam@aaronherres.com>
2019-08-14 09:20:06 -0700

    fetcher is ghetto, but works

--- scripts/get-corpus.js
+++ scripts/get-corpus.js
@@ -1,24 +1,26 @@
 
 const fetch = require('node-fetch')
-
+const _ = require('lodash')
 const { JSDOM } = require('jsdom');
-const baseUrl = "https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=futurama"
+const baseUrl = "https://www.springfieldspringfield.co.uk/"
 
-async function getEpisodeNames() {
-    const res = await fetch(baseUrl)
+async function getEpisodeList() {
+    const res = await fetch(baseUrl + "view_episode_scripts.php?tv-show=futurama")
     const text = await res.text()
     const dom = new JSDOM(text)
     const lElems = dom.window.document.querySelectorAll('.season-episode-title')
     const links = []
     lElems.forEach(e => links.push(e.getAttribute('href')))
-    console.log(links)
+    return links
 }
 
 async function getScript(url) {
+    url = baseUrl + url
     const res = await fetch(url)
     const text = await res.text()      
     const dom = new JSDOM(text)
     const script = dom.window.document.querySelector('.scrolling-script-container').textContent
+    console.log(script.replace(/\|/gi, '\n'))
     return script
 }
 // async function getScripts() {
@@ -26,6 +28,10 @@ async function getScript(url) {
 //     console.log("gs",script)
 //     return script
 // }
+async function getScripts() {
+    const episodeList = await getEpisodeList()
+    const promises = _.map(episodeList, getScript)
+    return await Promise.all(promises)
+}
 
-console.log(getEpisodeNames())
-// console.log(getScripts())
+getScripts()
